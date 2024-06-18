1d56303d44ab7d62a8bed00c3c54ba9fb8a3cd8b
priyankathomxs <200587559@student.georgianc.on.ca>
2024-06-18 00:02:10 -0400

    updated server

--- server.ts
+++ server.ts
@@ -6,7 +6,7 @@
 
 import app from './Server/Config/app';
 import debug from 'debug';
-debug('ice5:server');
+debug('Asssignment2:server');
 import http from 'http';
 import { HttpError } from 'http-errors';
 import { AddressInfo } from 'net';
